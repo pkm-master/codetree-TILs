798d4dd74a774443145b82d13915f0477c5d5554
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-07-04 16:14:52 +0900

    Title: 원소의 합이 0, Time: 1969ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240704/원소의 합이 0/the-sum-of-the-elements-is-0.java
+++ 240704/원소의 합이 0/the-sum-of-the-elements-is-0.java
@@ -3,37 +3,54 @@ import java.util.*;
 
 
 public class Main {
+
+    public static void hash_fill(Map<Integer,Integer> hash,int key){
+        if (hash.containsKey(key)){
+            hash.replace(key, hash.get(key)+1);
+        } else {
+            hash.put(key,1);
+        }
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
         
-        int[][] arr = new int[4][n];
-        Map<Integer,Integer> hash = new HashMap<>();
+        Map<Integer,Integer> hash1 = new HashMap<>();
+        Map<Integer,Integer> hash2 = new HashMap<>();
+        Map<Integer,Integer> hash3 = new HashMap<>();
+        Map<Integer,Integer> hash4 = new HashMap<>();
 
-        for (int i = 0 ; i < 4 ; i++){
-            StringTokenizer stk = new StringTokenizer(br.readLine());
-            for (int j = 0 ; j < n ; j++){
-                arr[i][j] = Integer.parseInt(stk.nextToken());
-                if (i == 3){
-                    if (hash.containsKey(arr[i][j])){
-                        hash.replace(arr[i][j], hash.get(arr[i][j])+1);
-                    } else {
-                        hash.put(arr[i][j],1);
-                    }
-                }
-            }
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n ; i++){
+            hash_fill(hash1,Integer.parseInt(stk.nextToken()));
+        }
+
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n ; i++){
+            hash_fill(hash2,Integer.parseInt(stk.nextToken()));
+        }
+
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n ; i++){
+            hash_fill(hash3,Integer.parseInt(stk.nextToken()));
+        }
+
+        stk = new StringTokenizer(br.readLine());
+        for (int i = 0 ; i < n ; i++){
+            hash_fill(hash4,Integer.parseInt(stk.nextToken()));
         }
 
         int ans = 0;
-        for (int i = 0 ; i < n ; i++){
-            for (int j = 0 ; j < n ; j++){
-                for (int k = 0 ; k < n ; k++){
-                    ans += hash.getOrDefault(-arr[0][i]-arr[1][j]-arr[2][k],0);
+
+        for (int key1 : hash1.keySet()){
+            for (int key2 : hash2.keySet()){
+                for (int key3 : hash3.keySet()){
+                    ans += hash1.get(key1) * hash2.get(key2) * hash3.get(key3) * hash4.getOrDefault(-key1-key2-key3,0);
                 }
             }
         }
-
         System.out.println(ans);
     }
 }
