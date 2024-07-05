74bfbbb972f60deea59a48087c4e705d0511fa34
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-07-05 14:55:19 +0900

    Title: 정수 찾기, Time: 1064ms, Memory: 37MB, Status: Passed - CodeTree

---
+++ 240705/정수 찾기/find-an-integer.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        HashSet<Long> hash = new HashSet<>();
+
+        for(int i = 0 ; i < n ; i++){
+            long ele = Long.parseLong(stk.nextToken());
+            hash.add(ele);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        stk = new StringTokenizer(br.readLine());
+
+        for (int j = 0 ; j < m ; j++){
+            long ele = Long.parseLong(stk.nextToken());
+            if (hash.contains(ele)){
+                System.out.println("1");
+            }else{
+                System.out.println("0");
+            }
+        }
+
+    }
+}
