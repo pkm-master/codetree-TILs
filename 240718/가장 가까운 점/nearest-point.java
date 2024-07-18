86b31b64590d3a8746ef6b04832350e1b76725e5
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-07-18 10:06:24 +0900

    Title: 가장 가까운 점, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240718/가장 가까운 점/nearest-point.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class Pair implements Comparable<Pair>{
+    int x,y;
+    public Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Pair p){
+        return Math.abs(this.x) + Math.abs(this.y) - Math.abs(p.x) - Math.abs(p.y);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+
+        int n = Integer.parseInt(stk.nextToken());
+        int m = Integer.parseInt(stk.nextToken());
+
+        for (int i=0 ; i<n ; i++){
+            stk = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(stk.nextToken());
+            int y = Integer.parseInt(stk.nextToken());
+            q.add(new Pair(x,y));
+            
+        }
+
+        for (int i=0 ; i<m ; i++){
+            Pair p = q.poll();
+            p.x += 2;
+            p.y += 2;
+            q.add(p);
+        }
+
+        System.out.println((q.peek().x)+" "+(q.peek().y));
+        
+    }
+}
