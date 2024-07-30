183f9631b14f798d1ce7d4c9b5f71fa74db60e33
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-07-30 09:14:21 +0900

    Title: 정원 입장은 선착순, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240730/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+class Pair implements Comparable<Pair>{
+    int x,y;
+    public Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+
+    }
+
+    @Override
+    public int compareTo(Pair p){
+        if (this.x > p.x){
+            return 1;
+        } else if (this.x == p.x && this.y > p.y){
+            return 1;
+        } else {
+            return -1;
+        }
+        
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Pair> not_arrive = new PriorityQueue<>();
+        PriorityQueue<Pair> waiting = new PriorityQueue<>();
+        int[] Arrive_time = new int[n];
+        int[] Enter_time = new int[n];
+        int[] spend_time = new int[n];
+        int current_time = 0;
+
+        for (int i=0 ; i<n ; i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stk.nextToken());
+            int t = Integer.parseInt(stk.nextToken());
+            spend_time[i] = t;
+
+            not_arrive.add(new Pair(a,i));
+        }
+
+        Pair Arrived = not_arrive.poll();
+        current_time += Arrived.x;
+        waiting.add(new Pair(Arrived.y, Arrived.x));
+        while (not_arrive.peek().x == current_time ){
+            Arrived = not_arrive.poll();
+            waiting.add(new Pair(Arrived.y, Arrived.x));
+        }
+
+        while (!not_arrive.isEmpty() || !waiting.isEmpty()){
+            if (!waiting.isEmpty()){
+                Pair Enter = waiting.poll();
+                Arrive_time[Enter.x] = Enter.y;
+                Enter_time[Enter.x] = current_time;
+                
+                current_time += spend_time[Enter.x];
+                while (!not_arrive.isEmpty() && not_arrive.peek().x <= current_time){
+                    Arrived = not_arrive.poll();
+                    waiting.add(new Pair(Arrived.y, Arrived.x));
+                }
+
+            } else{
+                Arrived = not_arrive.poll();
+                current_time = Arrived.x;
+                waiting.add(new Pair(Arrived.y, Arrived.x));
+                while (!not_arrive.isEmpty() && not_arrive.peek().x == current_time ){
+                    Arrived = not_arrive.poll();
+                    waiting.add(new Pair(Arrived.y, Arrived.x));
+                }
+            }
+        }
+
+        int max_wait = 0;
+        for (int i=0 ; i<n ; i++){
+            int waiting_time = Enter_time[i] - Arrive_time[i];
+            if (waiting_time > max_wait) max_wait = waiting_time;
+        }
+
+        System.out.println(max_wait);
+    }
+
+}
