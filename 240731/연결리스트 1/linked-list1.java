d7bc5a2988ca7d5d3bbb8ad80a4cd6a945c7ba30
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-07-31 17:47:50 +0900

    Title: 연결리스트 1, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240731/연결리스트 1/linked-list1.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+class Node{
+    String data;
+    Node prev,next;
+
+    public Node(String data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+
+    public void insertprev(Node node){
+        this.prev.next = node;
+        node.prev = this.prev;
+
+        this.prev = node;
+        node.next = this;
+
+    }
+
+    public void insertback(Node node){
+        this.next.prev = node;
+        node.next = this.next;
+
+        this.next = node;
+        node.prev = this;
+        
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s_init = br.readLine();
+        Node curr = new Node(s_init);
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=0 ; i<n ; i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            method = stk.nextToken();
+            if (method.equals("1")){
+                value = stk.nextToken();
+                
+            }else if (method.equals("2")){
+                value = stk.nextToken();
+
+            }else if (method.equals("3")){
+                if (curr.prev != null) curr = curr.prev;
+            }else if (method.equals("4")){
+                if (curr.next != null) curr = curr.next;
+            }
+
+        }
+
+
+    }
+}
