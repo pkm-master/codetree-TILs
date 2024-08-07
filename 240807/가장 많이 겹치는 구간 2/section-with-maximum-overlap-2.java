78e212ad98c0ee3e6f8a703906c675328477f834
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-07 22:13:29 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 463ms, Memory: 23MB, Status: Wrong Answer - CodeTree

---
+++ 240807/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        HashMap<Integer,Integer> line = new HashMap<>();
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(int i=0; i<n; i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(stk.nextToken());
+            int e = Integer.parseInt(stk.nextToken());
+            line.put(s,1);
+            line.put(e,-1);
+            q.add(s);
+            q.add(e);
+
+        }
+
+
+        int sums = 0;
+        int max_value = 0;
+        int max_cnt = 1;
+        
+        while (!q.isEmpty()){
+            int point = q.poll();
+            sums += line.get(point);
+            if (max_value<sums){
+                max_value = sums;
+                max_cnt = 1;
+            }else if (max_value==sums){
+                max_cnt += 1;
+            }
+
+        }
+        System.out.println(max_cnt);
+    }
+}
