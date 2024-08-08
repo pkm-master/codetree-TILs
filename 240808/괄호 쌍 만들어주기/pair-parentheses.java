b835d405b9082a0cb86c8db2edee001f1d0c2ad8
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-08 22:19:08 +0900

    Title: 괄호 쌍 만들어주기, Time: 160ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240808/괄호 쌍 만들어주기/pair-parentheses.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        ArrayList<Integer> ls = new ArrayList<>();
+        ArrayList<Integer> rs = new ArrayList<>();
+
+        for (int i=1; i<str.length(); i++){
+            char before = str.charAt(i-1);
+            char current = str.charAt(i);
+            if (current == '(' && before == '('){
+                ls.add(i);
+            }else if (current == ')' && before == ')'){
+                rs.add(i);
+            }
+        }
+
+        int[] biggers = new int[ls.size()];
+
+        int j=0;
+        int sums = 0;
+        for (int i=0;i<ls.size();i++){
+
+            while( j < rs.size() && ls.get(i)>rs.get(j) ){
+                j++;
+            }
+            biggers[i] = rs.size() - j;
+            sums += biggers[i];
+        }
+
+        System.out.println(sums);
+        
+
+    }
+}
