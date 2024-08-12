77b48f6f5e2694c929871b6a33c535863da68918
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-12 12:25:20 +0900

    Title: 가장 짧은 부분합, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240812/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int s = Integer.parseInt(stk.nextToken());
+
+        int[] arr = new int[n];
+        stk = new StringTokenizer(br.readLine());
+
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
+        }
+        
+        int sums = 0;
+        int ans = 1000000;
+        int j = 0;
+
+        for (int i=0; i<n; i++){
+            while (j<n && sums < s){
+                sums+=arr[j];
+                j++;
+
+            }
+
+            ans = Math.min(ans, j-i+1);
+            sums -= arr[i];
+
+        }
+        System.out.println(ans);
+    }
+}
