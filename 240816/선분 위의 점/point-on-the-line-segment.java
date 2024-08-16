bc63b0dc81ad8de0652c8403f148e0e97fef92a1
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-16 13:33:52 +0900

    Title: 선분 위의 점, Time: 2633ms, Memory: 89MB, Status: Wrong Answer - CodeTree

--- 240816/선분 위의 점/point-on-the-line-segment.java
+++ 240816/선분 위의 점/point-on-the-line-segment.java
@@ -23,45 +23,47 @@ public class Main {
         int m = Integer.parseInt(stk.nextToken());
 
         stk = new StringTokenizer(br.readLine());
-        int[] points = new int[n];
-        HashSet<Integer> pointsContain = new HashSet<>();
+        ArrayList<Long> points = new ArrayList<>();
+        HashSet<Long> pointsContain = new HashSet<>();
 
         for(int i=0; i<n; i++){
-            int point = Integer.parseInt(stk.nextToken());
-            points[i] = point;
+            long point = Long.parseLong(stk.nextToken());
+            points.add(point);
             pointsContain.add(point);
         }
 
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        HashMap<Integer,Integer> Line = new HashMap<>();
-        int[] lines = new int[m];
+        Collections.sort(points);
+
+        PriorityQueue<Long> q = new PriorityQueue<>();
+        HashMap<Long,Long> Line = new HashMap<>();
+        long[] lines = new long[m];
 
 
         for (int j=0; j<m; j++){
             stk = new StringTokenizer(br.readLine());
-            int st = Integer.parseInt(stk.nextToken());
-            int end = Integer.parseInt(stk.nextToken());
+            long st = Long.parseLong(stk.nextToken());
+            long end = Long.parseLong(stk.nextToken());
             q.add(st);
             q.add(end);
             Line.put(st,end);
             lines[j] = st;
         }
 
-        HashMap<Integer,Integer> point_n = new HashMap<>();
+        HashMap<Long,Integer> point_n = new HashMap<>();
 
         int i=0;
         while (!q.isEmpty()){
-            int value = q.poll();
-            while ( i<n && points[i]<value){
+            long value = q.poll();
+            while ( i<n && points.get(i)<value){
                 i++;
             }
             point_n.put(value,i);
         }
 
         for (int j=0; j<m; j++){
-            int st = lines[j];
-            int end = Line.get(st);
-            int ans = point_n.get(end)-point_n.get(st);
+            long st = lines[j];
+            long end = Line.get(st);
+            long ans = point_n.get(end)-point_n.get(st);
             if (pointsContain.contains(end)) ans +=1;
             System.out.println(ans);
 
