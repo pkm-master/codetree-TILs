068f06b7b4be23320cf743fa8e8b3c5a97d75684
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-17 15:47:04 +0900

    Title: 선분 위의 점, Time: 275ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240817/선분 위의 점/point-on-the-line-segment.java
+++ 240817/선분 위의 점/point-on-the-line-segment.java
@@ -2,68 +2,77 @@ import java.io.*;
 import java.util.*;
 
 
-class Line{
-    long s,e;
-    
-    public Line(long s, long e){
-        this.s = s;
-        this.e = e;
-
-    }
-}
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static int max = 100000;
+    public static int n,m;
+    public static int[] arr;
 
+    // value보다 작은 것 중에서 가장 큰 곳에 있는것
+    public static int find_lowerbound(int value){
+        int st = 0;
+        int end = n-1;
+        int ans = n;
+        
+        while (st<=end){
+
+            int mid = (st+end)/2;
+            if (arr[mid]>=value){
+                end = mid-1;
+                ans = Math.min(ans,mid);
+
+            } else{
+                st = mid+1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int find_upperbound(int value){
+        int st = 0;
+        int end = n-1;
+        int ans = n;
+
+        while (st<=end){
+            // System.out.println('a');
+            int mid = (st+end)/2;
+            if (arr[mid]<=value){
+                st = mid+1;
+
+            } else{
+                end = mid-1;
+                ans = Math.min(ans,mid);
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(stk.nextToken());
-        int m = Integer.parseInt(stk.nextToken());
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
 
         stk = new StringTokenizer(br.readLine());
-        ArrayList<Long> points = new ArrayList<>();
-        HashSet<Long> pointsContain = new HashSet<>();
+        arr = new int[n];
 
-        for(int i=0; i<n; i++){
-            long point = Long.parseLong(stk.nextToken());
-            points.add(point);
-            pointsContain.add(point);
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
         }
 
-        Collections.sort(points);
-
-        PriorityQueue<Long> q = new PriorityQueue<>();
-        Line[] lines = new Line[m];
-
-        for (int j=0; j<m; j++){
+        for (int i=0; i<m; i++){
             stk = new StringTokenizer(br.readLine());
-            long st = Long.parseLong(stk.nextToken());
-            long end = Long.parseLong(stk.nextToken());
-            q.add(st);
-            q.add(end);
-            lines[j] = new Line(st,end);
-        }
+            int st = Integer.parseInt(stk.nextToken());
+            int end = Integer.parseInt(stk.nextToken());
+            System.out.println(find_upperbound(end)-find_lowerbound(st));
+            // System.out.println(find_lowerbound(st));
 
-        HashMap<Long,Integer> point_n = new HashMap<>();
-
-        int i=0;
-        while (!q.isEmpty()){
-            long value = q.poll();
-            while ( i<n && points.get(i)<value){
-                i++;
-            }
-            point_n.put(value,i);
-        }
-
-        for (int j=0; j<m; j++){
-            long st = lines[j].s;
-            long end = lines[j].e;
-            long ans = point_n.get(end)-point_n.get(st);
-            if (pointsContain.contains(end)) ans +=1;
-            System.out.println(ans);
 
         }
 
-        // System.out.println(point_n);
+
+
 
     }
 }
