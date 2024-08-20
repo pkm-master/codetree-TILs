f74082b6179f0163b116ac6067ab1229e0bfa974
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-20 14:33:28 +0900

    Title: 격자 칠하기 2, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240820/격자 칠하기 2/painting-the-grid-2.java
+++ 240820/격자 칠하기 2/painting-the-grid-2.java
@@ -14,59 +14,51 @@ public class Main {
     public static int n;
     public static int[][] arr;
     public static Pair[] dir = {new Pair(0,1),new Pair(0,-1),new Pair(1,0),new Pair(-1,0)};
+    public static boolean[][] visited;
 
-    public static int count(int[][] visited){
-        int sum = 0;
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                sum += visited[i][j];
+    public static int coloring (int value, Pair st){
+        if (visited[st.x][st.y]){
+            return 0;
+        }
+        visited[st.x][st.y] = true;
+        int cnt = 1;
+        for (int i=0; i<4; i++){
+            Pair next = new Pair(st.x+dir[i].x, st.y+dir[i].y);
+            if (next.x<n && next.y<n && next.x>=0 && next.y>=0 && !visited[next.x][next.y] && Math.abs(arr[next.x][next.y]-arr[st.x][st.y]) <= value){
+                cnt += coloring(value,next);
             }
         }
-        return sum;
-    }
-
-    public static int color (int value, Pair st){
-
-        int[][] visited = new int[n][n];
-        Queue<Pair> q = new LinkedList<>();
-        visited[st.x][st.y] = 1;
-        q.add(st);
-
-        while (!q.isEmpty()){
-            Pair curr = q.poll();
-            for (int i=0; i<4; i++){
-                Pair next = new Pair(curr.x + dir[i].x, curr.y + dir[i].y);
-                if (next.x<n && next.y<n && next.x>=0 && next.y>=0 && visited[next.x][next.y]==0 && Math.abs(arr[next.x][next.y]-arr[curr.x][curr.y]) <= value){
-                    q.add(next);
-                    visited[next.x][next.y] = 1;
-                }
-            }
-
-        }
-
-        return count(visited);
-
+        return cnt;
     }
 
     public static boolean is_possible(int value){
-        int total = 0;
         for (int i=0; i<n; i++){
             for (int j=0; j<n; j++){
-                int part = color(value, new Pair(i,j));
-                total = Math.max(part,total);
+                visited[i][j] = false;
             }
         }
-        if (total>=Math.pow(n,2)/2+Math.pow(n,2)%2){
-            return true;
+        Pair st = new Pair(0,0);
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                st.x = i;
+                st.y = j;
+                // int part = color(value, st);
+                int part = coloring(value, st);
+                if (part>=Math.pow(n,2)/2+Math.pow(n,2)%2){
+                    return true;
+                }
+
+            }
         }
+
         return false;
     }
 
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
+        visited = new boolean[n][n];
 
         for (int i=0; i<n; i++){
             StringTokenizer stk = new StringTokenizer(br.readLine());
