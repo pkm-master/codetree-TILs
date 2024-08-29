69cbe9bdaf5d4a9b70e351f7325710d694d1ff2d
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-29 12:01:02 +0900

    Title: 수 채우기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240829/수 채우기/fill-in-number.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int st5 = n/5;
+        while ((n - st5 * 5) % 2 == 1){
+            st5--;
+        }
+
+        int n2 = (n - st5 * 5) / 2;
+        System.out.println(st5 + n2);
+
+    }
+}
