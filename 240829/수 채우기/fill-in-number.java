feee184d8cd31f2853dcd1474bfa5167093e023c
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-08-29 12:02:50 +0900

    Title: 수 채우기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240829/수 채우기/fill-in-number.java
+++ 240829/수 채우기/fill-in-number.java
@@ -8,12 +8,17 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int st5 = n/5;
-        while ((n - st5 * 5) % 2 == 1){
+        while ((n - st5 * 5) % 2 == 1 && st5>0){
             st5--;
         }
 
         int n2 = (n - st5 * 5) / 2;
-        System.out.println(st5 + n2);
+
+        if (n2 * 2 + st5 *5 == n){
+            System.out.println(st5 + n2);
+        } else{
+            System.out.println(-1);
+        }
 
     }
 }
