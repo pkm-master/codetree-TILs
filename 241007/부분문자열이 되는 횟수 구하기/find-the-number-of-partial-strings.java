76f43555fa2038a74b45757601441a774eac98b6
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-10-07 15:28:34 +0900

    Title: 부분문자열이 되는 횟수 구하기, Time: 267ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241007/부분문자열이 되는 횟수 구하기/find-the-number-of-partial-strings.java
+++ 241007/부분문자열이 되는 횟수 구하기/find-the-number-of-partial-strings.java
@@ -4,13 +4,23 @@ import java.util.*;
 
 public class Main {
     
-    public static String[] As;
+    public static int[] del;
+    public static boolean[] skip;
 
-    public static boolean IsBoobun(String A, String B){
+    public static boolean IsBoobun(String A, String B, int mid){
         // A랑 B를 비교해서 B가 A의 부분문자열인지 확인
+        
         int bOrder = 0;
         int bLength = B.length();
+
+        for (int i=1; i<=mid; i++){
+            skip[del[i]-1] = true;
+        }
+
+
         for (int i=0; i<A.length(); i++){
+            if (skip[i]) continue;
+
             if (B.charAt(bOrder)==A.charAt(i)){
                 bOrder++;
             }
@@ -26,28 +36,32 @@ public class Main {
         String A = br.readLine();
         String B = br.readLine();
 
-        As = new String[A.length()+1];
         StringTokenizer stk = new StringTokenizer(br.readLine());
         
-        As[0] = A;
+        del = new int[A.length()+1];
+        skip = new boolean[A.length()+1];
 
-        for (int i=0; i<A.length(); i++){
-            int removeIdx = Integer.parseInt(stk.nextToken())-1;
-            A = A.substring(0,removeIdx)+"*"+A.substring(removeIdx+1);
-            As[i+1] = A;
+        for (int i=1; i<= A.length(); i++){
+            del[i] = Integer.parseInt(stk.nextToken());
+
         }
 
         int st = 0;
         int end = A.length();
         int ans = 0;
 
+        
         while (st<=end){
             int mid = (st+end)/2;
-            if (IsBoobun(As[mid],B)){
+            if (IsBoobun(A,B, mid)){
                 st = mid+1;
                 ans = Math.max(ans, mid);
             } else{
                 end = mid-1;
+            }
+
+            for (int i=1; i<=mid; i++){
+                skip[del[i]-1] = false;
             }
         }
 
