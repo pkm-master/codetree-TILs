02086bbb3a264232453a5a295262cfd465688bc9
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-10-19 21:09:53 +0900

    Title: 크기가 n인 순열, Time: 1044ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 241019/크기가 n인 순열/n-permutation.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static boolean[] visited;
+    public static int[] selected;
+    public static int n;
+    public static void dfs(int idx){
+        if(idx == n){
+            for (int i=0; i<n; i++){
+                System.out.print(selected[i]+" ");
+            }
+            System.out.println();
+        } else{
+            for (int i=1; i<=n; i++){
+                if (!visited[i-1]){
+                    visited[i-1] = true;
+                    selected[idx] = i;
+                    dfs(idx+1);
+                    visited[i-1] = false;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n];
+        selected = new int[n];
+
+        dfs(0);
+    }
+}
