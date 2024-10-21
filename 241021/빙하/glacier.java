42c6d0697af1a5debafccd3d52ef81db36d67355
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-10-21 14:25:11 +0900

    Title: 빙하, Time: 468ms, Memory: 47MB, Status: Passed - CodeTree

--- 241021/빙하/glacier.java
+++ 241021/빙하/glacier.java
@@ -21,7 +21,7 @@ public class Main {
     public static int howManyLeft(){
         int ans  = 0;
         for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
+            for (int j=0; j<m; j++){
                 ans += iceBerg[i][j];
             }
         }
@@ -50,6 +50,7 @@ public class Main {
                 }
             }
         }
+
 
     }
 
