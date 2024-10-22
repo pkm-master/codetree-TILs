75e6af2bc1e9becc127fdc63ccaa4b4f8c47a276
pkm-master <156387263+pkm-master@users.noreply.github.com>
2024-10-22 12:19:51 +0900

    Title: xor 결과 최대 만들기, Time: 3014ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 241022/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] numbers;
+    public static int[] result;
+    public static boolean[] visited;
+    
+    public static int n,ans,m;
+    public static void dfs(int idx){
+        if (idx == m){
+            // XOR 한 수 구하고 ans랑 비교
+            int resultN = result[0];
+            for (int i=1; i<m; i++){
+                resultN = resultN ^ result[i];
+            }
+            ans = Math.max(ans,resultN);           
+            return;
+        }
+        for (int i=0; i<n; i++){
+            if (!visited[i]){
+                result[idx] = numbers[i];
+                visited[i] = true;
+                dfs(idx+1);
+                visited[i] = false;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
+        result = new int[m];
+        numbers = new int[n];
+        stk = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++){
+            numbers[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        visited = new boolean[n];
+
+        dfs(0);
+        System.out.println(ans);
+        // n개의 음이아닌 정수
+        // m개를 뽑아 XOR
+
+    }
+}
